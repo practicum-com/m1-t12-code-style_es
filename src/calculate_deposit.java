01d342dc0fe5bed8483b07ae0b74632330bc59b9
Volan-de-krot <115557216+kv-zayceva@users.noreply.github.com>
2023-03-30 18:09:58 +0300

    calculate_deposit.java
    Vnesla esp

--- src/calculate_deposit.java
+++ src/calculate_deposit.java
@@ -17,17 +17,17 @@ double Calculate_Complex_Percent_Function(double a, double y,int d ) {
     void do_important_job( )
     {
       int period, action ;
-        Scanner abcdef = new Scanner(System.in); System.out.println("Enter the amount of the deposit in dollars:");
-      int amount = abcdef.nextInt(); System.out.println("Enter the length of the deposit in years:");
+        Scanner abcdef = new Scanner(System.in); System.out.println("Ingresa la cantidad del depósito en dólares:");
+      int amount = abcdef.nextInt(); System.out.println("Ingresa la duración del depósito en años:");
         period = abcdef.nextInt( );
-      System.out.println   ("Select the type of the deposit, 1 — a deposit with an ordinary interest, 2 — a capitalization deposit:");
+      System.out.println   ("Selecciona el tipo de depósito, 1 — un depósito con una tasa de interés ordinaria, 2 — un depósito con capitalización:");
         action = abcdef.nextInt();
         double outDoubleVar = 0;
         if (action ==1) outDoubleVar = Calculate_Simple_Percent_Function(amount, 0.06, period);
         else if (action == 2)
         {
             outDoubleVar = Calculate_Complex_Percent_Function(amount, 0.06, period); }
-        System.out.println("The yield of the deposit: " + amount + " will turn into " + outDoubleVar + " in " + period + " years." );
+        System.out.println("La rentabilidad del depósito: " + amount + " se convertirá en " + outDoubleVar + " en " + period + " años." );
     }
 public static void main(String[] args)
     {
